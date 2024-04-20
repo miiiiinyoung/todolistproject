b57c52ba92f1bad2a065dfedb94397f0226387f1
miiiiinyoung <164984438+miiiiinyoung@users.noreply.github.com>
2024-04-21 03:36:03 +0900

    Update views.py

--- todoproject/todo/views.py
+++ todoproject/todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
-from django.http import JsonResponse
+
 
 def task_list(request):
     tasks = Task.objects.all()
